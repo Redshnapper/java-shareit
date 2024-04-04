1729915b956efaf3e3d438ccc801f2b81f4b938d
Андрей <a.yahin@tematelecom.ru>
2024-04-04 15:33:01 +0300

    Удалил неиспользуемые импорты

--- server/src/main/java/ru/practicum/shareit/handler/ApplicationExceptionHandler.java
+++ server/src/main/java/ru/practicum/shareit/handler/ApplicationExceptionHandler.java
@@ -2,15 +2,11 @@ package ru.practicum.shareit.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.exception.BadRequestException;
 import ru.practicum.shareit.exception.NotFoundException;
-
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
